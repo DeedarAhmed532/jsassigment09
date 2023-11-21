3ff40caad78aafdaa5242dbe961b3de1da1768a8
Deedar Ahmed <deedarahmed532@gmail.com>
2023-11-21 07:31:04 +0500

    0o

---
+++ app.js
@@ -0,0 +1,14 @@
+var myName = "my name is deedar ahmed"
+var myAge = "22 years old"
+var course = "Certified Mobile Application Development"
+ 
+var email = "my email address is www@example.com"
+var education = "I am trying to learn from the book A smarter way to learn JavaScript"
+alert(myName);
+alert(myAge);
+alert(course);
+ 
+alert(email);
+alert(education);
+ 
+ 
